b2f735fc0cb44c837e5b4c17ec9c092be547b462
no_name <hieuddeu@gmail.com>
2026-05-10 22:52:59 +0700

    add leaderboard

--- src/main/java/domain/system_study_api/dto/quiz_result/TopStudentDTO.java
+++ src/main/java/domain/system_study_api/dto/quiz_result/TopStudentDTO.java
@@ -2,7 +2,9 @@ package domain.system_study_api.dto.quiz_result;
 
 import java.util.UUID;
 
+
 public interface TopStudentDTO {
+    UUID getId();
     Integer getScore();
     Integer getTime_spent();
     String getFirst_name();
@@ -10,10 +12,11 @@ public interface TopStudentDTO {
     String getLevel_name();
     String getClass_name();
     String getSchool_name();
+    String getPart_name();
 
     // Default method để gộp tên học sinh trực tiếp
     default String getStudentName() {
-        return getFirst_name() + " " + getLast_name();
+        return getLast_name() + " " + getFirst_name();
     }
 
 }
--- src/main/java/domain/system_study_api/service/quiz_submit/QuizSubmitServiceImpl.java
+++ src/main/java/domain/system_study_api/service/quiz_submit/QuizSubmitServiceImpl.java
@@ -2,11 +2,13 @@ package domain.system_study_api.service.quiz_submit;
 
 import domain.system_study_api.dto.quiz_result.QuizResultRequestDTO;
 import domain.system_study_api.dto.quiz_result.QuizResultResponseDTO;
+import domain.system_study_api.dto.quiz_result.TopStudentDTO;
 import domain.system_study_api.entity.Part;
 import domain.system_study_api.entity.QuizResult;
 import domain.system_study_api.entity.Student;
 import domain.system_study_api.listener.QuizSubmittedEvent;
 import domain.system_study_api.mapper.QuizResultMapper;
+import domain.system_study_api.repository.LeaderBoardRepository;
 import domain.system_study_api.repository.PartRepository;
 import domain.system_study_api.repository.QuizSubmitRepository;
 import domain.system_study_api.repository.StudentRepository;
@@ -18,13 +20,17 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class QuizSubmitServiceImpl implements QuizSubmitService {
 
     private final QuizSubmitRepository quizRepository;
+    private final LeaderBoardRepository lbRepository;
     private final StudentRepository studentRepository;
+    private final QuizResultMapper leaderboardMapper;
     private final SimpMessagingTemplate messagingTemplate;
     private final PartRepository partRepository;
     private final QuizResultMapper quizResultMapper;
@@ -46,18 +52,18 @@ public class QuizSubmitServiceImpl implements QuizSubmitService {
         result = quizRepository.save(result);
 
         // 2. Gửi tín hiệu WebSocket để update Leaderboard Realtime
+        List<TopStudentDTO> leaderboardData = lbRepository.findByClassIdAndPartId(request.getPartId(), request.getClassId());
+        log.info("leaderboardData={}", leaderboardData);
         // Chúng ta gửi về topic của lớp đó để các bạn cùng lớp thấy ngay
-        String topic = "/topic/leaderboard/" + student.getClassroom().getId();
-        messagingTemplate.convertAndSend(topic, "UPDATE_COMMAND");
+        // 3. "Bắn" data qua WebSocket tới kênh /topic/leaderboard
+        messagingTemplate.convertAndSend("/topic/leaderboard", leaderboardData);
 
-        // 3. Ghi lên Google Sheet (Chạy bất đồng bộ - Async)
-//        googleSheetService.updateScoreOnSheetAsync(result);
-        // 2. Bắn Event thay vì gọi trực tiếp Async
+
+        // 4. Ghi lên Google Sheet (Chạy bất đồng bộ - Async)
+        // Bắn Event thay vì gọi trực tiếp Async
         // Spring sẽ giữ Event này lại cho đến khi Transaction thành công
         eventPublisher.publishEvent(new QuizSubmittedEvent(result.getId()));
 
         return quizResultMapper.mapToResponseDto(result);
     }
-
-
 }
