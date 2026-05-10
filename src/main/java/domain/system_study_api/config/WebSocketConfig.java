9efa960a18269687d65ebf165238b29143d94ab7
no_name <hieuddeu@gmail.com>
2026-05-10 23:33:31 +0700

    config cors

--- src/main/java/domain/system_study_api/config/WebSocketConfig.java
+++ src/main/java/domain/system_study_api/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${FE_URL}")
+    @Value("${fe.url}")
     private String feUrl;
 
     @Override
